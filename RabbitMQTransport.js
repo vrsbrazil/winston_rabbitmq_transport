67123871c3a47b62f516910f8425259f9309a08c
Vinicius Santos <vrsbrazil@gmail.com>
2018-05-21 10:22:46 -0300

    Transport Creation

---
+++ RabbitMQTransport.js
@@ -0,0 +1,69 @@
+var amqp = require('amqplib/callback_api');
+var Transport = require('winston-transport');
+
+module.exports = class RabbitMQTransport extends Transport {
+
+  constructor(opts) {
+    super(opts);
+
+    if(!opts.url){
+      throw new Error("url is required");
+    }
+
+    if(!opts.queue){
+      throw new Error("queue is required");
+    }
+
+    this.queue = opts.queue;
+    this.pLogger = new Promise(function(resolve, reject){
+
+      amqp.connect(opts.url, function(err, conn) {
+
+        if(err){
+          return reject(err);
+        }
+
+        conn.createChannel(function(err, ch) {
+
+          if(err){
+            return reject(err);
+          }
+
+          var q = opts.queue;
+
+          ch.assertQueue(q, {durable: false});
+
+          resolve(ch);
+
+        });
+
+      });
+
+    });
+
+  }
+
+  log(info, message) {
+
+    var pLogger = this.pLogger;
+    var queue = this.queue;
+
+    setImmediate(function () {
+      pLogger.then(function(channel){
+
+        var log = JSON.stringify({
+          level: info,
+          log: message
+        });
+
+        channel.sendToQueue(queue, new Buffer(log));
+
+      },function(err){
+        console.log("logger error", err);
+      });
+    });
+
+
+
+  }
+};
---
+++ index.js
@@ -0,0 +1,3 @@
+var RabbitMQTransport = require("./RabbitMQTransport");
+
+module.exports = RabbitMQTransport;
