75f78185bac063211498019ad01e381088590d41
Vinicius Santos <vrsbrazil@gmail.com>
2018-11-08 10:14:58 -0200

    New origin opt feature added

--- RabbitMQTransport.js
+++ RabbitMQTransport.js
@@ -15,6 +15,7 @@ module.exports = class RabbitMQTransport extends Transport {
     }
 
     this.queue = opts.queue;
+    this.origin = opts.origin;
     this.pLogger = new Promise(function(resolve, reject){
 
       amqp.connect(opts.url, function(err, conn) {
@@ -47,13 +48,15 @@ module.exports = class RabbitMQTransport extends Transport {
 
     var pLogger = this.pLogger;
     var queue = this.queue;
+    var origin = this.origin;
 
     setImmediate(function () {
       pLogger.then(function(channel){
 
         var log = JSON.stringify({
           level: info,
-          log: message
+          log: message,
+          "origin": origin
         });
 
         channel.sendToQueue(queue, new Buffer(log));
